b78728f1e8ad56438dfcafeb35d658b7ad085b89
Ter <smarttapon.lim@gmail.com>
2025-10-18 02:51:26 +0700

    mandatory

---
+++ for-42/src_bonus/pipex_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: slimvutt <slimvut@fpgij;dgj;ds.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/18 01:25:36 by slimvutt          #+#    #+#             */
+/*   Updated: 2025/10/18 01:25:36 by slimvutt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H_BONUS
+# define PIPEX_H_BONUS
+# define BUFFER_P 500
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+//# include <sys/wait.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+
+
+
+
+
+#endif
